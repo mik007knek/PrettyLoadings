0bcde37b27fe970f93964a1a7986110f06957d68
Knek <example@gmail.com>
2020-09-07 20:08:21 +0300

    adding info about project

---
+++ __init__.py
@@ -0,0 +1,20 @@
+r"""
+This is PrettyLoadings lib by mik007knek, and it is almost the most useless lib!
+
+Version = 0.0.0.1
+
+Loadings:
+-Text Expansion (t->te->tes->test)
+-Symbol Expansion (%->%%->%%%->%%%%)
+-Dot Circling ('-'->\->|->/)
+-Line Circling ('-'->\\->|||->////)
+(all examples are shown as 1 time loop)
+
+In next versions there will be more loadings and (maybe) adaptive loadings
+for tkinter and more!
+
+Soon, I can (maybe not) upload this project to PyPi!
+
+P. S.: In each func in console_load, you can see `clear: bool = False`. I used it for people who have macOS or Linux
+P. S. 2: Sorry for my english, i don't know it very well, i'm from Russia)
+"""
